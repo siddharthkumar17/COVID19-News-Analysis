57c5276b773fe3eec46e82f936d2a7ed2eacf926
Siddharth Kumar <sidkumar@live.com>
2020-04-05 12:42:24 -0500

    create gitignore and new file

--- CBCAnalysis.py
+++ CBCAnalysis.py
@@ -10,18 +10,16 @@ df['publish_date'] = pd.to_datetime(df['publish_date'])
 df = df[df['publish_date']>pd.to_datetime('2019')]
 
 sid = SentimentIntensityAnalyzer()
-
 data = pd.DataFrame(columns=['date', 'negative', 'positive'])
+
 for row in df.itertuples():
-   
     text = row.text
     ss = sid.polarity_scores(text)
-    
     data = data.append({
       'date':row.publish_date,
       'negative':ss['neg'],
       'positive':ss['pos']
       },ignore_index=True)
+
 data = data.groupby('date').mean()
-print(data)
-data.plot(figsize=(20,10))
+data.plot(figsize=(20,10),title='CBC Dataset Sentiment Analysis')
---
+++ NewsAPIAnalysis.py
@@ -0,0 +1,5 @@
+import os
+from dotenv import load_dotenv
+from pyspark import SparkContext, SparkConf
+
+load_dotenv()

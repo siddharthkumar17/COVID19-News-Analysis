376875729a08b4a78a327d3f8aa0981ba61d75cb
Siddharth Kumar <sidkumar@live.com>
2020-04-05 16:46:20 -0500

    spark plot

--- NewsAPIAnalysis.py
+++ NewsAPIAnalysis.py
@@ -2,6 +2,7 @@ from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 import json
 from pyspark.sql.types import *
+from pyspark.sql.functions import *
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer 
 spark = SparkSession.builder.master("local").appName("News Analysis").getOrCreate()
 sc = spark.sparkContext
@@ -10,17 +11,17 @@ df = spark.read.json("/FileStore/tables/data.json")
 
 sid = SentimentIntensityAnalyzer()
 
-schema = StructType([StructField("date", StringType(), True),StructField("negative", DecimalType(), True),StructField("positive", DecimalType(), True)])
+schema = StructType([StructField("date", DateType(), True),StructField("negative", DecimalType(), True),StructField("positive", DecimalType(), True)])
 data = sqlContext.createDataFrame(sc.emptyRDD(), schema)
 
 for row in df.toLocalIterator():
     text = row.description
     ss = sid.polarity_scores(text)
     newrow = spark.createDataFrame([{
-      'date':row.publishedAt,
+      'date':row.publishedAt.split('T')[0],
       'negative':ss['neg'],
       'positive':ss['pos']
       }])
     data = data.union(newrow)
 
-print(data.first())
+display(data.groupBy('date').avg())
